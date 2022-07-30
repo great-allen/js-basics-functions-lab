31d28171a35b9faf46b42449fb24dcd59753fb90
QunFan <allenfan1012@gmail.com>
2022-07-30 22:57:07 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    if (blockNumber > 42) {
+        return blockNumber - 42;
+    } else if (blockNumber < 42) {
+        return 42 - blockNumber;
+    }
+}
+
+function distanceFromHqInFeet(blockNumber) {
+   return distanceFromHqInBlocks(blockNumber)*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start) {
+    return ((destination-start) * 264);
+    } else if (destination < start) {
+        return ((start - destination) * 264);
+    }
+}
+    function  calculatesFarePrice(start, destination) {
+        let travelDistance = distanceTravelledInFeet(start, destination)
+        if (travelDistance <= 400) {
+            return 0;
+        } else if (travelDistance > 400 && travelDistance<=2000) {
+            return 0.02 * (travelDistance - 400);
+        } else if (travelDistance > 2000 && travelDistance <=2500) {
+            return 25;
+        } else {
+            return 'cannot travel that far';
+        }
+       
+}
